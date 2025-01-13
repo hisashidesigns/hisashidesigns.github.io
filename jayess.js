a270d4a36e36cd0361aebcd63aecde84952e84c1
malkelSchoolAcc <malkel808@outlook.com>
2025-01-13 15:52:31 -0500

    Update jayess.js

--- jayess.js
+++ jayess.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById("myCanvas");
 function draw() {
-	var ctx = canvas.getContext("2D");
+	var ctx = canvas.getContext("2d");
 	ctx.canvas.width = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
 	ctx.fillStyle = "red";
-	ctx.fillRect = (0, 0, 175, 50);
+	ctx.fillRect(0, 0, 175, 50);
 }
 
 draw();
